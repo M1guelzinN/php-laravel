b81415aa0297697efeed6083887fdd43c3db78e4
M1guelzinN <antonio.jobs1397@gmail.com>
2022-08-10 15:30:02 -0300

    aula 28

---
+++ app/Http/Controllers/ServicoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServicoController extends Controller
+{
+    public function Servico(){
+        echo 'pagina de produtos';
+    }
+}
+
