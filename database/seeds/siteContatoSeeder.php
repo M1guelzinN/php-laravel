5a7f58d2e2d7b39d125b162a58ff0e68187c6a25
M1guelzinN <antonio.jobs1397@gmail.com>
2022-08-29 16:33:56 -0300

    aula 119

--- database/seeds/siteContatoSeeder.php
+++ database/seeds/siteContatoSeeder.php
@@ -12,20 +12,22 @@ class siteContatoSeeder extends Seeder
      */
     public function run()
     {
-        $contato = new site_contato();
-        $contato->nome = 'Carlos'; 
-        $contato->telefone = '(11)9999-2222'; 
-        $contato->email = 'carlos@contato.com'; 
-        $contato->motivo_contato = 1; 
-        $contato->mensagem = 'gostei muito';
-        $contato->save();
+        factory(site_contato::class, 100)->create();
 
-        $contato = new site_contato();
-        $contato->nome = 'Maria'; 
-        $contato->telefone = '(11)9999-2222'; 
-        $contato->email = 'maria@contato.com'; 
-        $contato->motivo_contato = 3; 
-        $contato->mensagem = 'nossaaaaa que site sensacional';
-        $contato->save();
+        // $contato = new site_contato();
+        // $contato->nome = 'Carlos'; 
+        // $contato->telefone = '(11)9999-2222'; 
+        // $contato->email = 'carlos@contato.com'; 
+        // $contato->motivo_contato = 1; 
+        // $contato->mensagem = 'gostei muito';
+        // $contato->save();
+
+        // $contato = new site_contato();
+        // $contato->nome = 'Maria'; 
+        // $contato->telefone = '(11)9999-2222'; 
+        // $contato->email = 'maria@contato.com'; 
+        // $contato->motivo_contato = 3; 
+        // $contato->mensagem = 'nossaaaaa que site sensacional';
+        // $contato->save();
     }
 }
